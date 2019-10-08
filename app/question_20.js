5285ba5678740e9001a551ca4b46510f5efedb36
Gerald <geraldinyiama@gmail.com>
2019-10-08 17:18:42 +0100

    finished

---
+++ app/question_20.js
@@ -0,0 +1,13 @@
+age = prompt("Input age","");
+
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Did parents allow you?');
+//     }
+// }
+// checkAge(age);
+
+
+(age > 18)? true : (confirm('Did your parents allow you?'));
